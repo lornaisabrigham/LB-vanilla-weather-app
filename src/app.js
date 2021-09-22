eaab57498921e99007087d832e32edf3a1fab7bf
lornaisabrigham <lbrigham747@gmail.com>
2021-09-22 14:57:14 +0100

    Weekly forecast made
    HTML moved to js
    Loop set up to run each day

--- src/app.js
+++ src/app.js
@@ -21,6 +21,34 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+              <div class="weather-forecast-date">
+                ${day}
+              </div>
+              <img src="https://openweathermap.org/img/wn/50d@2x.png"
+              alt=""
+              width="40"/>
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max">
+                  18° 
+                </span>
+                <span class="weather-forecast-temperature-min">
+                  12°
+                </span>
+              </div>
+            </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -88,3 +116,4 @@ let celciusElement = document.querySelector("#celcius");
 celciusElement.addEventListener("click", toCelcius);
 
 searchCity("brighton");
+displayForecast();
