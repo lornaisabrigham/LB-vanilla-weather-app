669e68c64239f3ee0c77175049970f09661e3263
lornaisabrigham <lbrigham747@gmail.com>
2021-09-28 15:52:13 +0100

    Forecast linked to api
    js used to predict upcoming weather

--- src/app.js
+++ src/app.js
@@ -21,31 +21,40 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast(response) {
-  console.log(response.data.daily);
+function formatForecastDays(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+  return days[day];
+}
 
+function displayForecast(response) {
+  let forecastDays = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
+  forecastDays.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
               <div class="weather-forecast-date">
-                ${day}
+                ${formatForecastDays(forecastDay.dt)}
               </div>
-              <img src="https://openweathermap.org/img/wn/50d@2x.png"
+              <img src="https://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png"
               alt=""
               width="40"/>
               <div class="weather-forecast-temperatures">
                 <span class="weather-forecast-temperature-max">
-                  18° 
+                  ${Math.round(forecastDay.temp.max)}° |
                 </span>
                 <span class="weather-forecast-temperature-min">
-                  12°
+                  ${Math.round(forecastDay.temp.min)}°
                 </span>
               </div>
             </div>`;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
